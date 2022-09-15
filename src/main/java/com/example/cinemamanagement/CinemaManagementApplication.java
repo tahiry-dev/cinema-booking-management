66af9895216c1797b004433c31d8b8291f35d7b3
tahiry-dev <tahirycontact@gmail.com>
2022-09-15 20:21:15 +0300

    feat: configure the JPA connection

--- src/main/java/com/example/cinemamanagement/CinemaManagementApplication.java
+++ src/main/java/com/example/cinemamanagement/CinemaManagementApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CinemaManagementApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaManagementApplication.class, args);
 	}
